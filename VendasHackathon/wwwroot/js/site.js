a2128b599566991eb28f02e7ef60441d484f73be
tiagoeuzebiocordeiro <tiago.ec2709@gmail.com>
2024-02-24 13:28:14 -0300

    Mudanças layout index de cliente + adição de datatables e tradução das datatables.

--- VendasHackathon/wwwroot/js/site.js
+++ VendasHackathon/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#tabela-formatada').DataTable();
+});
 
-// Write your JavaScript code.
+$(document).ready(function () {
+    $('#tabela-formatada').DataTable({
+        language: {
+            "decimal": "",
+            "emptyTable": "Nenhum dado disponível.",
+            "info": "Mostrando START para END de TOTAL resultados",
+            "infoEmpty": "Mostrando 0 para 0 de 0 resultados",
+            "infoFiltered": "(filtrado de MAX resultados totais)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar MENU resultados",
+            "loadingRecords": "Carregando...",
+            "processing": "",
+            "search": "Buscar:",
+            "zeroRecords": "Não foram encontrados resultados correspondentes",
+            "paginate": {
+                "first": "Primeira",
+                "last": "Última",
+                "next": "Próxima",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "orderable": "Ordene por essa coluna",
+                "orderableReverse": "Ordem inversa por essa coluna"
+            }
+        }
+    });
