ce2967fbe28aeb65549440644f9e264e06829cbf
tiagoeuzebiocordeiro <tiago.ec2709@gmail.com>
2024-02-24 13:36:54 -0300

    POST (Create) de ClienteModel

--- VendasHackathon/Controllers/ClienteController.cs
+++ VendasHackathon/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VendasHackathon.Models;
 using VendasHackathon.Repositories.Cliente;
 
 namespace VendasHackathon.Controllers
@@ -15,6 +16,22 @@ namespace VendasHackathon.Controllers
         {
             var clientes = _clienteRepository.GetAll();
             return View(clientes);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ClienteModel cliente) 
+        {
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            _clienteRepository.Add(cliente);
+            return RedirectToAction("Index");
         }
     }
 }
