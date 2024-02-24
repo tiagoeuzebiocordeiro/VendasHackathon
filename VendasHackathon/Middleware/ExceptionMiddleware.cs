8c406027f5ff9ca1e8e9f6b45fbec2f2b9f537b1
Romulo Cordeiro <romulocordeiro.ti@gmail.com>
2024-02-24 10:18:49 -0300

    Classe para tratamento de exceções: ExceptionMiddleware adicionado

---
+++ VendasHackathon/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace VendasHackathon.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500;
+
+                var response = new
+                {
+                    Title = ex.Message,
+                    Status = 500,
+                    Detail = _env.IsDevelopment() ? ex.StackTrace?.ToString() : null
+                };
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
--- VendasHackathon/Program.cs
+++ VendasHackathon/Program.cs
@@ -1,9 +1,14 @@
+using VendasHackathon.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Middleware para tratamento de exceções
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
